b01dc3c83bc66ef2ca7a8a2864d8f679bad9ecca
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-12-21 12:52:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,27 @@ fetch(url)
         const width = 800;
         const height = 500;
         const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-        const svg = d3.select("#scatterplot")
+        const svg = d3.select("#scatter-plot")
             .attr("width", width)
             .attr("height", height);
 
         const xScale = d3.scaleTime()
-            .domain([d3.min(data, d => new Date(d.year)),
-            d3.max(data, d => new Date(d.year))])
+            .domain([d3.min(data, d => new Date(d.Year - 1, 0)), d3.max(data, d => new Date(d.Year + 1, 0))])
             .range([margin.left, width - margin.right]);
 
         const yScale = d3.scaleTime()
-            .domain([d3.mindata, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60), d3.max(data, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60))])
+            .domain([d3.min(data, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60)), d3.max(data, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60))])
             .range([height - margin.bottom, margin.top]);
+
+        svg.append("g")
+            .attr("id", "x-axis")
+            .attr("transform", `translate(0, ${height - margin.bottom})`)
+            .call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y")));
+
+        svg.append("g")
+            .attr("id", "y-axis")
+            .attr("transform", `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S")));
 
     })
     .catch(error => console.error('Error fetching data:', error));
