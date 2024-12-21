75b6cd7d29d6501b4216293ab9ddd19d3cd90348
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-12-21 12:39:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.error('Error fetching data:', error));
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        const width = 800;
+        const height = 500;
+        const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+        const svg = d3.select("#scatterplot")
+            .attr("width", width)
+            .attr("height", height);
+
+
+
+
+    })
+    .catch(error => console.error('Error fetching data:', error));
