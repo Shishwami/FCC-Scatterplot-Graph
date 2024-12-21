a599931a44fa96bc24449569a0a574dc71c926bf
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-12-21 12:47:12 +0800

    Update

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ fetch(url)
             .attr("width", width)
             .attr("height", height);
 
+        const xScale = d3.scaleTime()
+            .domain([d3.min(data, d => new Date(d.year)),
+            d3.max(data, d => new Date(d.year))])
+            .range([margin.left, width - margin.right]);
 
-
+        const yScale = d3.scaleTime()
+            .domain([d3.mindata, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60), d3.max(data, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60))])
+            .range([height - margin.bottom, margin.top]);
 
     })
     .catch(error => console.error('Error fetching data:', error));
