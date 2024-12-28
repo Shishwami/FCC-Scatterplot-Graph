6ce7c96d0cbef793a176af9e195022afbf951554
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-12-28 14:41:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ fetch(url)
 
         const width = 800;
         const height = 500;
-        const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+        const margin = { top: 50, right: 50, bottom: 50, left: 70 };
+
+        const xRange = d3.extent(data, d => d.Year);
+        const yRange = d3.extent(data, d => d.Seconds);
+
+        const xOffsetDynamic = (xRange[1] - xRange[0]) * 0.05;
+        const yOffsetDynamic = (yRange[1] - yRange[0]) * 0.11;
+
         const svg = d3.select("#scatter-plot")
             .attr("width", width)
             .attr("height", height);
 
         const xScale = d3.scaleTime()
-            .domain([d3.min(data, d => new Date(d.Year - 1, 0)), d3.max(data, d => new Date(d.Year + 1, 0))])
+            .domain([
+                new Date(xRange[0] - xOffsetDynamic, 0),
+                new Date(xRange[1] + xOffsetDynamic, 0)])
             .range([margin.left, width - margin.right]);
 
         const yScale = d3.scaleTime()
-            .domain([d3.max(data, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60)), d3.min(data, d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60))])
+            .domain([
+                new Date(0, 0, 0, 0, Math.ceil(yRange[1] / 60), yRange[1] % 60 - yOffsetDynamic),
+                new Date(0, 0, 0, 0, Math.floor(yRange[0] / 60), yRange[0] % 60 - yOffsetDynamic)])
             .range([height - margin.bottom, margin.top]);
 
         svg.append("g")
@@ -40,14 +51,15 @@ fetch(url)
             .attr("r", 5)
             .attr("data-xvalue", d => d.Year)
             .attr("data-yvalue", d => new Date(0, 0, 0, 0, d.Seconds / 60, d.Seconds % 60))
-            .style("fill", d =>
-                d.Doping == "" ? "orange" : "blue"
-            )
+            .style("fill", d => d.Doping == "" ? "orange" : "blue")
             .on("mouseover", (event, d) => {
                 d3.select("#tooltip")
                     .style("visibility", "visible")
                     .attr("data-year", d.Year)
-                    .html(`Year: ${d.Year}<br>Time: ${d.Time}`);
+                    .html(`Year: ${d.Year}<br>Time: ${d.Time}`)
+                    .style("left", `${event.pageX + 10}px`)
+                    .style("top", `${event.pageY + 10}px`);
+
             })
             .on("mouseout", () => {
                 d3.select("#tooltip").style("visibility", "hidden");
